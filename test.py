20eb31880c90c8321f0b9986d3aae4b42756062b
chaitu2023 <123951493+chaitu2023@users.noreply.github.com>
2023-12-07 11:49:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,14 +10,20 @@ def main():
     driver = webdriver.Chrome(options=options)
     
     try:
-        driver.get("http://3.110.92.113")  # Make sure to prefix the URL with http:// or https://
+        driver.get("http://3.110.92.113")  # Replace with your URL
 
-        # Wait for the element to be present for a maximum of 15 seconds
+        # Wait for the welcome element to be present for a maximum of 15 seconds
         wait = WebDriverWait(driver, 15)
         welcome_element = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[40]/div[2]/div/div[2]/div[4]/div/div/div[1]')))
 
         if welcome_element:
             print("Page loaded successfully")
+
+            # Find and click the Full Body Test button
+            full_body_test_button = driver.find_element(By.XPATH, '/html/body/div[40]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/a[1]/div[1]')
+            full_body_test_button.click()
+
+            print("Selected Full Body Test")
         else:
             print("Page not loaded")
     
