59212f3a16e30ef7d5799068d111fb780628b833
chaitu2023 <123951493+chaitu2023@users.noreply.github.com>
2023-12-07 15:22:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def main():
     driver = webdriver.Chrome(options=options)
     
     try:
-        driver.get("http://3.110.46.249")  # Replace with your URL
+        driver.get("http://3.110.92.113")  # Replace with your URL
 
         # Wait for the welcome element to be present for a maximum of 15 seconds
         wait = WebDriverWait(driver, 15)
@@ -19,23 +19,11 @@ def main():
         if welcome_element:
             print("Page loaded successfully")
 
-            # Add an implicit wait to give time for the elements to load
-            driver.implicitly_wait(10)
+            # Find and click the Full Body Test button
+            full_body_test_button = driver.find_element(By.XPATH, '/html/body/div[40]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/a[1]')
+            full_body_test_button.click()
 
-            # Check if there are any iframes and switch to them
-            iframes = driver.find_elements(By.TAG_NAME, 'iframe')
-            for iframe in iframes:
-                driver.switch_to.frame(iframe)
-                try:
-                    # Find and click the Full Body Test button within the iframe
-                    full_body_test_button = driver.find_element(By.XPATH, '/html/body/div[40]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/a[1]/div[1]')
-                    full_body_test_button.click()
-                    print("Selected Full Body Test")
-                    break
-                except Exception as e:
-                    # Element not found in this iframe, continue to the next
-                    driver.switch_to.default_content()
-                    continue
+            print("Selected Full Body Test")
         else:
             print("Page not loaded")
     
