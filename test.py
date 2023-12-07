2556cfa2053e112bdd8f83786b51479cad0ba601
chaitu2023 <123951493+chaitu2023@users.noreply.github.com>
2023-12-07 17:30:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def click_element(driver, xpath):
-    try:
-        element = driver.find_element(By.XPATH, xpath)
-        element.click()
-    except Exception as e:
-        print(f"Error while clicking element: {str(e)}")
-
 def main():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
@@ -18,7 +11,6 @@ def main():
     
     try:
         driver.get("http://3.110.46.249")  # Replace with your URL
-        full_body_test_button_xpath = '/html/body/div[40]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/a[2]'
 
         # Wait for the welcome element to be present for a maximum of 15 seconds
         wait = WebDriverWait(driver, 15)
@@ -27,10 +19,25 @@ def main():
         if welcome_element:
             print("Page loaded successfully")
 
-            # Find and click the Full Body Test button using the click_element function
-            click_element(driver, full_body_test_button_xpath)
+            # Look for iframes
+            iframes = driver.find_elements(By.TAG_NAME, 'iframe')
 
-            print("Selected Full Body Test")
+            # If iframes are found, iterate through and try to locate the element in each iframe
+            for iframe in iframes:
+                driver.switch_to.frame(iframe)
+                try:
+                    # Find and click the Full Body Test button using an explicit wait
+                    full_body_test_button_xpath = '/html/body/div[40]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/a[2]'
+                    full_body_test_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, full_body_test_button_xpath)))
+                    full_body_test_button.click()
+                    print("Selected Full Body Test")
+                    break
+                except Exception as e:
+                    print("Element not found in this iframe:", str(e))
+                    driver.switch_to.default_content()
+                    continue
+            else:
+                print("Failed to select Full Body Test")
         else:
             print("Page not loaded")
     
