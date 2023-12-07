829f3764ddd2313feee7bfc54f02641247962f0e
chaitu2023 <123951493+chaitu2023@users.noreply.github.com>
2023-12-07 17:48:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -19,27 +19,11 @@ def main():
         if welcome_element:
             print("Page loaded successfully")
 
-            # Look for iframes
-            iframes = driver.find_elements(By.TAG_NAME, 'iframe')
-
-            # If iframes are found, iterate through and try to locate the element in each iframe
-            for iframe in iframes:
-                driver.switch_to.frame(iframe)
-                try:
-                    # Find and click the Full Body Test button using an explicit wait
-                    full_body_test_button_xpath = '/html/body/div[40]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/a[1]/div[1]'
-                    full_body_test_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, full_body_test_button_xpath)))
-                    full_body_test_button.click()
-                    print("Selected Full Body Test")
-                    break
-                except Exception as e:
-                    print("Element not found in this iframe:", str(e))
-                    # Optionally, you can print the current page source for debugging purposes:
-                    # print(driver.page_source)
-                    driver.switch_to.default_content()
-                    continue
-            else:
-                print("Failed to select Full Body Test")
+            # Locate and click the Full Body Test button without iframe handling
+            full_body_test_button_xpath = '/html/body/div[40]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/a[1]/div[1]'
+            full_body_test_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, full_body_test_button_xpath)))
+            full_body_test_button.click()
+            print("Selected Full Body Test")
         else:
             print("Page not loaded")
     
